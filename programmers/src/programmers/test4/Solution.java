694bbd1e25ec84d04d0ffa4284508d4f22c08058
leechangtaek <59005174+leechangtaek@users.noreply.github.com>
2020-05-06 13:21:14 +0900

    no message

--- programmers/src/programmers/test4/Solution.java
+++ programmers/src/programmers/test4/Solution.java
@@ -12,14 +12,42 @@ public class Solution {
 	public static void main(String[] args) {
 		Solution s = new Solution();
 		int a[][] = {{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,1,0}};
+		int b[][] = {{0,0,1,1},{1,1,1,1}};
 		
 		System.out.println("결과:"+s.rectangle(a));
+		System.out.println("결과:"+s.rectangle(b));
 	}
 	
 	public int rectangle(int [][]board) {
 		int answer = 0;
+		int [][] arr = new int[board.length+1][board[0].length+1];
+		
+		for(int i=0; i<board.length; i++) {
+			for(int j=0; j<board[0].length; j++) {
+				if(board[i][j]==1) {
+					arr[i+1][j+1]=1;
+				}else {
+					arr[i+1][j+1]=0;
+				}
+			}
+		}
+		
+		for(int i=1; i<arr.length; i++) {
+			for(int j=1; j<arr[0].length; j++) {
+				if(arr[i][j]==1) {
+					int min= Math.min(arr[i-1][j], arr[i][j-1]);
+					min = Math.min(arr[i-1][j-1], min);
+					
+					arr[i][j]=min+1;
+					answer = Math.max(answer, arr[i][j]);
+				}
+				System.out.println(answer);
+			}
+		}
+		
+		
 		System.out.println(Arrays.deepToString(board));
 		
-		return answer;
+		return answer*answer;
 	}
 }
