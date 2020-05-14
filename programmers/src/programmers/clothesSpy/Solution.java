aff060267522592ebf66f43ed6671a77fe6a5b4b
leechangtaek <59005174+leechangtaek@users.noreply.github.com>
2020-05-14 15:28:36 +0900

    스파이 랜덤 옷

---
+++ programmers/src/programmers/clothesSpy/Solution.java
@@ -0,0 +1,37 @@
+package programmers.clothesSpy;
+/*
+ * 스파이들은 매일 다른 옷을 조합하여 입어 자신을 위장합니다.
+
+예를 들어 스파이가 가진 옷이 아래와 같고 오늘 스파이가 동그란 안경, 
+긴 코트, 파란색 티셔츠를 입었다면 다음날은 청바지를 추가로 입거나 동그란 안경 대신 검정 선글라스를 착용하거나 해야 합니다.
+ */
+import java.util.HashMap;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+
+		String a [][] = {{"yellow_hat", "headgear"},{"blue_sunglasses", "eyewear"},{"green_turban", "headgear"}};//5
+		String b [][]= {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};//3
+		System.out.println("결과:"+s.solution(a));
+		System.out.println("결과:"+s.solution(b));
+	}
+	 public int solution(String[][] clothes) {
+		 int answer = 1;
+		 
+		 HashMap<String,Integer> map = new HashMap<>();
+		 for(int i=0; i<clothes.length; i++) {
+			 map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+		 }
+		 for(String key : map.keySet()) {
+			 answer *= (map.get(key)+1);
+		 }
+		 answer = answer-1;
+		 
+		 System.out.println(map);
+		 
+		 return answer;
+	 }
+
+}
