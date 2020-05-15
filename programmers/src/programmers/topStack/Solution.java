14742a95ea093779caf34d0576f4d6a250b1ed1c
leechangtaek <59005174+leechangtaek@users.noreply.github.com>
2020-05-15 16:36:08 +0900

    no message

--- programmers/src/programmers/topStack/Solution.java
+++ programmers/src/programmers/topStack/Solution.java
@@ -20,39 +20,40 @@ public class Solution {
 	public int[] solution(int [] heights) {
 		int [] answer = new int[heights.length];
 		
-//		HashMap<Integer, Integer> map = new HashMap<>();
-//		Stack<Integer> stack = new Stack<>();
-//		for(int i = heights.length - 1; i >= 0; i--){
-//			if(stack.isEmpty()){
-//				stack.push(heights[i]);
-//			}else{
-//				Stack<Integer> tmp = new Stack<>();
-//				while(!stack.isEmpty() && stack.peek() >= heights[i]){
-//					tmp.push(stack.pop());
-//				}
-//				while(!stack.isEmpty() && stack.peek() < heights[i]){
-//					map.put(stack.pop(), i + 1);
-//				}
-//				while(!tmp.isEmpty()){
-//					stack.push(tmp.pop());
-//				}
-//				stack.push(heights[i]);
-//			}
-//		}
-//
-//		for(int i = heights.length - 1; i >= 0; i--){
-//			if(map.containsKey(heights[i])){
-//				answer[i] = map.remove(heights[i]);
-//			}
-//		}
-		
-		for(int i=0; i<heights.length; i++) {
-			for(int j=i+1; j<heights.length; j++) {
-				if(heights[i]>heights[j]) {
-					answer[j]=i+1;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		Stack<Integer> stack = new Stack<>();
+		for(int i = heights.length - 1; i >= 0; i--){
+			if(stack.isEmpty()){
+				stack.push(heights[i]);
+			}else{
+				Stack<Integer> tmp = new Stack<>();
+				while(!stack.isEmpty() && stack.peek() >= heights[i]){
+					tmp.push(stack.pop());
 				}
+				while(!stack.isEmpty() && stack.peek() < heights[i]){
+					map.put(stack.pop(), i + 1);
+				}
+				while(!tmp.isEmpty()){
+					stack.push(tmp.pop());
+				}
+				stack.push(heights[i]);
 			}
 		}
+
+		for(int i = heights.length - 1; i >= 0; i--){
+			if(map.containsKey(heights[i])){
+				answer[i] = map.remove(heights[i]);
+			}
+		}
+		
+//		stack안쓰기
+//		for(int i=0; i<heights.length; i++) {
+//			for(int j=i+1; j<heights.length; j++) {
+//				if(heights[i]>heights[j]) {
+//					answer[j]=i+1;
+//				}
+//			}
+//		}
 		
 		return answer;
 		
