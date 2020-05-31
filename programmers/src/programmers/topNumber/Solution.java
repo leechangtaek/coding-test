4e1a00f34b14b6f3cfcb0602bd5efc28549d0270
leechangtaek <ckdxor1014@naver.com>
2020-05-31 16:43:24 +0900

    [정렬]가장 큰 수

--- programmers/src/programmers/topNumber/Solution.java
+++ programmers/src/programmers/topNumber/Solution.java
@@ -1,5 +1,8 @@
 package programmers.topNumber;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
 
 	public static void main(String[] args) {
@@ -14,7 +17,26 @@ public class Solution {
 	public String solution(int[] numbers) {
         String answer = "";
         
+        String[] arr = new String[numbers.length];
         
+        for(int i=0; i<arr.length; i++) {
+        	arr[i] = (String.valueOf(numbers[i]));
+        }
+        
+        Arrays.sort(arr,new Comparator<String>() {
+        	@Override
+        	public int compare(String s1, String s2) {
+        		return(s2+s1).compareTo(s1+s2);
+        	}
+		});
+        
+        if(arr[0].equals("0")) return "0";
+        
+        for(int i=0; i<arr.length; i++) {
+        	answer+=arr[i];
+        }
+        
+        System.out.println(Arrays.toString(arr));
         return answer;
     }
 
