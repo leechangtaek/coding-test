01bc48fdff4e040e21818f8aaa996e6ea4794a12
leechangtaek <ckdxor1014@naver.com>
2020-05-31 16:23:52 +0900

    가장 큰 수

---
+++ programmers/src/programmers/topNumber/Solution.java
@@ -0,0 +1,21 @@
+package programmers.topNumber;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Solution s = new Solution();
+		int a [] = {6, 10, 2};
+		int b[] = {3, 30, 34, 5, 9};
+		
+		System.out.println("결과:"+s.solution(a));
+		System.out.println("결과:"+s.solution(b));
+	}
+	public String solution(int[] numbers) {
+        String answer = "";
+        
+        
+        return answer;
+    }
+
+}
