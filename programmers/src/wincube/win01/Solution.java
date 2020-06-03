285a048e7b29a3d5577be4d5c4f566fb94021e28
leechangtaek <ckdxor1014@naver.com>
2020-06-03 14:41:30 +0900

    no message

--- programmers/src/wincube/win01/Solution.java
+++ programmers/src/wincube/win01/Solution.java
@@ -2,6 +2,9 @@ package wincube.win01;
 
 import java.util.Arrays;
 
+/*
+ * 알파벳의 개수가 홀수개인 알파벳의 개수
+ */
 public class Solution {
 
 	public static void main(String[] args) {
@@ -19,10 +22,11 @@ public class Solution {
         char [] alpha = new char[26];
         int [] count = new int[26];
         
+        //알파벳을 배열에 담기
         for(int i=0; i<alpha.length; i++) {
         	alpha[i] = (char)(i+97);
         }
-        
+        System.out.println(Arrays.toString(alpha));
         for(int i=0; i<alpha.length; i++) {
         	for(int j=0; j<s.length(); j++) {
         		if(alpha[i]==s.charAt(j)) {
