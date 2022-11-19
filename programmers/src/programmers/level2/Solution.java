53d36742966dd504df73e19a6b2e54a5ede0504c
leechangtaek <ckdxor1014@naver.com>
2022-11-19 15:56:21 +0900

    우박수열과 정적분

---
+++ programmers/src/programmers/level2/Solution.java
@@ -0,0 +1,16 @@
+package programmers.kNumber;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+public class Solution2 {
+
+	public static void main(String[] args) {
+
+		int k = 5;
+		int ranges[][] = {{0,0},{0,-1},{2,-3},{3,-3}};
+
+	}
+
+}
